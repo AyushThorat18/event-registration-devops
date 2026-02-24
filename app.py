b78e38a8af0f12e728d5aae0a72ce4668dc502a6
Ayush Thorat <ayushthorat18@gmail.com>
2026-02-24 23:13:22 +0530

    Initial Flask CI/CD project

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        event = request.form.get("event")
+
+        return render_template(
+            "success.html",
+            name=name,
+            email=email,
+            event=event
+        )
+
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
